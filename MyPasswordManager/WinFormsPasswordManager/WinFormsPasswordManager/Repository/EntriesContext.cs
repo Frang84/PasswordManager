e95703ad382c0db14eaaa9562494e8769bf1c8ab
Frang84 <skorniak.skorniak@wp.pl>
2024-01-22 10:44:07 +0100

    opis

--- MyPasswordManager/WinFormsPasswordManager/WinFormsPasswordManager/Repository/EntriesContext.cs
+++ MyPasswordManager/WinFormsPasswordManager/WinFormsPasswordManager/Repository/EntriesContext.cs
@@ -14,15 +14,12 @@ namespace WinFormsPasswordManager.Repository
     public class EntriesContext : DbContext
     {
         public DbSet<Entry> Entries { get; set; }
-        
     
-      
-       
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connectionString = ConfigurationManager.ConnectionStrings["MyKey"].ConnectionString;
             optionsBuilder.UseSqlite(connectionString);
-            //optionsBuilder.UseSqlite(@"Data Source=C:\studia\rokIII\Vsemestr\wzorceProjektowe\Projekt\wszystko\PasswordManager\MyPasswordManager\WinFormsPasswordManager\WinFormsPasswordManager\Repository\entries.db;");
+           
         }
        
     }
